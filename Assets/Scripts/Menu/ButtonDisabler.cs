364d54189a475f019830c3705f267dd11939e839
nullsoftware <tumyrthebot@gmail.com>
2022-07-16 16:39:09 +0300

    Improved scene.

--- Assets/Scripts/Menu/ButtonDisabler.cs
+++ Assets/Scripts/Menu/ButtonDisabler.cs
@@ -5,12 +5,21 @@ using UnityEngine.UI;
 
 public class ButtonDisabler : MonoBehaviour
 {
+    [SerializeField] private ushort _levelNumber = 1;
+
     private Image _image;
+    private Button _btn;
 
     private void Start()
     {
         _image = GetComponent<Image>();
-        _image.color = Color.black;
+        _btn = GetComponent<Button>();
+
+        if (LevelData.Default.OpenedLevel < _levelNumber)
+        {
+            _image.color = Color.black;
+            _btn.interactable = false;
+        }
     }
 
     private void Enable()
